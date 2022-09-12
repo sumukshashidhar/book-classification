2f7cdb5764f9bab072ff7a32634fcf219a33d95a
Sumuk Shashidhar <sumukuuu@gmail.com>
2022-09-12 02:50:21 +0000

    feat: add v2 of the classification algorithm

--- src/subroutines/classification_algorithm.py
+++ src/subroutines/classification_algorithm.py
@@ -1,27 +1,22 @@
 """
 """
+
 def evaluate_genre(description, keyword_list):
-    phrases_seen = set()
     point_scores = 0
     total_occurances = 0
+    unique_occurances = 0
     for keyword_tuple in keyword_list:
         keyword = keyword_tuple[0]
         points = keyword_tuple[1]
         keyword_occurances = description.count(keyword)
         if keyword_occurances > 0:
-            # we also need to check if its unique or not, and then add points based on that
-            if keyword not in phrases_seen:
-                phrases_seen.add(keyword)
-                point_scores += points
+            unique_occurances += 1
+            point_scores += points
             total_occurances += keyword_occurances
-    # calculate average point score (k) of all occurances
-    # look for a short circuit if either point scores or phrases len is 0
-    if point_scores == 0 or len(phrases_seen) == 0:
+    if unique_occurances == 0:
         return 0
-    k = point_scores / len(phrases_seen)
-    # return the genre score (n * k)
+    k = point_scores / unique_occurances
     return total_occurances * k
-
 
 def score(description, lookup_table):
     # set description to lowercase to avoid issues with matching
